3ac37e820f2703de8334c8709e9239a43e28c150
Mikael Cavalcanti <mcs11@cin.ufpe.br>
2022-12-15 00:49:16 -0300

    Scripts Update

---
+++ BinarySearch.hpp
@@ -0,0 +1,14 @@
+#ifndef BINARY_SEARCH
+#define BINARY_SEARCH
+
+template <class T>
+class BinarySearch
+{
+private:
+    /* data */
+public:
+    BinarySearch(/* args */);
+    ~BinarySearch();
+};
+
+#endif
---
+++ QuickSort.hpp
@@ -0,0 +1,14 @@
+#ifndef QUICK_SORT
+#define QUICK_SORT
+
+template <class T>
+class QuickSort
+{
+private:
+    /* data */
+public:
+    QuickSort(/* args */);
+    ~QuickSort();
+};
+
+#endif

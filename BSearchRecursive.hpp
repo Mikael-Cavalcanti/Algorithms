de4e804cce52e08e11134510e937abe274a16e7a
mcs11 <mcs11@g3c34.cin.ufpe.br>
2022-12-20 08:08:23 -0300

    Fix: BSearch Method

--- BSearchRecursive.hpp
+++ BSearchRecursive.hpp
@@ -7,22 +7,22 @@ class BSearchRecursive
 public:
     BSearchRecursive() {}
     ~BSearchRecursive() {}
-    int Search(T array[], int left, int right, T Key)
+    bool Search(T array[], int left, int right, T Key)
     {
         int middle;
 
         if (right >= left)
         {
             middle = (left + right) / 2;
-            if (Key = array[middle])
-                return middle;
+            if (Key == array[middle])
+                return true;
             if (Key < array[middle])
                 return Search(array, left, middle - 1, Key);
             else
                 return Search(array, middle + 1, right, Key);
         }
         else
-            return -1;
+            return false;
     }
 };
 

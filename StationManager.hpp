4e5a372170033f32c015b1557520e005ef729e3f
Mikael-Cavalcanti <mcs11@cin.ufpe.br>
2023-02-20 23:20:33 -0300

    comments

--- StationManager.hpp
+++ StationManager.hpp
@@ -17,10 +17,17 @@ private:
     Stack<T> *station;
 
 private:
+    void Init()
+    {
+        trainA = new LList<T>();
+        trainB = new LList<T>();
+    }
+
     void MakeTrain()
     {
         for (int i = 1; i <= sizeTrain; i++)
             trainA->Append(i);
+
         trainA->Print();
     }
 
@@ -40,11 +47,22 @@ public:
              << "finish Program" << endl;
     }
 
-    void Init()
-    {
-        trainA = new LList<T>();
-        trainB = new LList<T>();
-    }
+public:
+    /*
+    - array trem - stack station - array new trem
+
+    1 - pego o vagão;
+    - coloco na station;
+    - verifico se está na ordem;
+    - se estiver coloco no novo trem até terminar os trens da estaçao;
+    - caso a station não esteja vazia;
+    - caso a soma do tamanho da station + B = n, não tem solução;
+    2 - volto para o passo até que a station esteja vazia ou não tenha solução;
+
+    add in station
+    remove in station
+    verify order
+    */
 };
 
 #endif

63456b2a1b12f92df3950b6226619872966da1ec
mcs11 <mcs11@g3c17.cin.ufpe.br>
2023-03-23 11:54:39 -0300

    dijkstra update

--- Dijkstra.hpp
+++ Dijkstra.hpp
@@ -4,18 +4,42 @@
 #include <queue>
 #include "Graph.hpp"
 
-#define infinity
+#define infinity 9999999
+#define noParent -1
+
+using namespace std;
 
 class Dijkstra
 {
+private:
+    int *distance, *parent;
+
+private:
+    void Init(int size)
+    {
+        distance = new int[size];
+        parent = new int[size];
+    }
+
 public:
     Dijkstra() {}
-    ~Dijkstra() {}
+    ~Dijkstra()
+    {
+        delete[] distance;
+        delete[] parent;
+    }
 
 public:
     // vai servir para organizar a heap, apenas (predecessor, vertice, peso)
     void PathFind(Graph *g, const int start, int *d)
     {
+        for (int i = 0; i < g->Vertice(); i++)
+        {
+            distance[i] = infinity;
+            parent[i] = noParent;
+        }
+
+        priority_queue<int> pq;
     }
 };
 

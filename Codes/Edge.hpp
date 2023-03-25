46442f1c56d20522c209dc912b71e48febbd90c4
Mikael-Cavalcanti <mcs11@cin.ufpe.br>
2023-03-25 20:50:35 -0300

    Add Edge List adjacency Graph

---
+++ Codes/Edge.hpp
@@ -0,0 +1,28 @@
+#ifndef EDGE_ADJACENCY_LIST_GRAPH
+#define EDGE_ADJACENCY_LIST_GRAPH
+
+#include <limits>
+
+template <typename T>
+class Edge
+{
+private:
+    T vert;
+    int wt;
+
+public:
+    Edge()
+    {
+        vert = T();
+        wt = 0;
+    }
+    Edge(const T v, const int w)
+    {
+        vert = v;
+        wt = w;
+    }
+    const T Vertex() const { return vert; }
+    const int Weight() const { return wt; }
+};
+
+#endif

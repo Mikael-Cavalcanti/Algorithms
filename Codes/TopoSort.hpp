ea6f86e9e60cf846afaa59412559fa3a2332368d
Mikael-Cavalcanti <mcs11@cin.ufpe.br>
2023-04-30 15:52:28 -0300

    topSort

--- Codes/TopoSort.hpp
+++ Codes/TopoSort.hpp
@@ -12,44 +12,14 @@ using namespace std;
 
 class TopoSort
 {
-private:
-    stack<int> s;
-
 public:
-    TopoSort(Graph *g)
-    {
-        int n = g->GetVertices();
-        for (int i = 0; i < n; i++)
-        {
-            g->SetMark(i, unvisited);
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            if (!g->GetMark(i))
-                Sort(g, i);
-        }
-    }
-
-    void Sort(Graph *g, const int v)
+    void TopSort(Graph *g, const int v, stack<int> &s)
     {
         g->SetMark(v, visited);
         for (int w = g->First(v); w < g->GetVertices(); w = g->Next(v, w))
             if (!g->GetMark(w))
-                Sort(g, w);
+                TopSort(g, w, s);
         s.push(v);
-    }
-
-    stack<int> GetStack() const { return s; }
-
-    void Print()
-    {
-        while (!s.empty())
-        {
-            cout << s.top() << " ";
-            s.pop();
-        }
-        cout << endl;
     }
 };
 
